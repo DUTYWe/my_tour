2b131b1e620f72e08a90c555e6ff19f573b582f1
DUTYWe <dutycsd@qq.com>
2022-04-22 00:37:07 +0800

    add sql2struct command

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 ## word
 提供编码时的单词转换功能，包含转大写，转小写，下划线单词转大写/小写驼峰格式以及驼峰格式转下划线格式。
+
+## 2struct
+提供一种直接由数据库中表的信息生成model的功能
+* Flags:
+      --charset string    请输入数据库的编码 (default "utf8mb4")
+  -d, --db string         请输入数据库名称
+  -h, --help              help for 2struct
+      --host string       请输入数据库的HOST (default "127.0.0.1:3306")
+  -p, --password string   请输入数据库的密码
+  -t, --table string      请输入表名称
+      --type string       请输入数据库实例类型 (default "mysql")
+  -n, --username string   请输入数据库的账号 *
--- main.go
+++ main.go
@@ -11,3 +11,20 @@ func main() {
 		log.Fatalf("cmd.Execute err: %v", err)
 	}
 }
+
+// const templateText = `
+// Output 0: {{title .Name1}}
+// Output 1: {{title .Name2}}
+// Output 2: {{.Name3 | title}}
+// `
+
+// func main() {
+// 	funcMap := template.FuncMap{"title": strings.Title}
+// 	tpl, _ := template.New("go-programming-tour").Funcs(funcMap).Parse(templateText)
+// 	data := map[string]string{
+// 		"Name1": "go",
+// 		"Name2": "programming",
+// 		"Name3": "tour",
+// 	}
+// 	_ = tpl.Execute(os.Stdout, data)
+// }
